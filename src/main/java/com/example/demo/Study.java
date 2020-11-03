940c4f173972475f134539da13d48fb6b4b12040
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-11-03 20:52:04 +0900

    test : add test code in time_out and throw exception

--- src/main/java/com/example/demo/Study.java
+++ src/main/java/com/example/demo/Study.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 public class Study {
 
     private StudyStatus status;
 
-    @Min(1)
+    @Min(value = 0, message = "인원의 limit는 0보다 커야합니다.")
     private final int limit;
 
     public Study(int limit) {
+        if (limit < 0){
+            throw new IllegalArgumentException("Limit 는 0보다 커야한다.");
+        }
         this.status = StudyStatus.DRAFT;
         this.limit = limit;
     }
--- src/test/java/com/example/demo/StudyTest.java
+++ src/test/java/com/example/demo/StudyTest.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,17 +35,49 @@ class StudyTest {
         // Arrange
         Study study = new Study(10);
 
-        System.out.println("StudyTest.create_new_study");
-
         // Assert
         assertAll(
                 () -> assertNotNull(study),
-                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 만들었을 때는 "+StudyStatus.DRAFT+" 상태여야 한다."),
-                    // 단순히 문자열을 넘기는 방식으로 작성할 경우 성공 실패 여부와 상관없이 문자열 연산을 하지만
-                    // 람다식으로 만들었을 경우에는 해당 함수가 필요할 때만 호출되어 생성됨으로 자원을 아낄 수 있다.
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 만들었을 때는 "+StudyStatus.DRAFT+" 상태여야 한다."),
+                    // 단순히 문자열을 넘기는 방식으로 작성할 경우 해당 코드는 성공 실패 여부와 상관없이 문자열 연산을 하지만
+                    // 람다식으로 만들었을 경우에는 해당 함수가 필요할 때만 호출되어 생성됨으로 연산 자원을 아낄 수 있다.
                 () -> assertTrue(study.getLimit() > 0, "스터디 참석 가능 인원은 0보다 커야합니다.")
         );
+        System.out.println("StudyTest.create_new_study");
 
     }
 
+    @Test
+    @DisplayName("스터디 생성 시 ThrowException ")
+    void create_new_study_ThrowException() {
+        // assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+
+        // Arrange
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+
+        // Assert
+        assertEquals(message, "Limit 는 0보다 커야한다.");
+    }
+
+    @Test
+    @DisplayName("스터디 생성 시 ThrowException ")
+    void create_new_study_And_Timeout() {
+        assertTimeout(Duration.ofSeconds(1), () -> new Study(10));
+
+        assertTimeout(Duration.ofMillis(315), () -> {
+            new Study(10); // 9~14 Millis
+            Thread.sleep(300);
+        });
+
+        // 해당 기능의 경우 이 코드 블럭을 별도의 쓰레드로 동작한다.
+        // Thread Local 을 사용하는 코드가 내부에 존재한다면 예상치 못한 문제가 발생할 수 있다.
+        // Transaction 을 사용하는 코드가 내부에 있다면 DB에 해당 테스트 내용이 반영될 수 있다.
+        // Transaction 이 설정된 쓰레드가 아닌 별개의 쓰레드로 동작시키기 때문이다.
+        assertTimeoutPreemptively(Duration.ofMillis(315), () -> {
+            new Study(10); // 9~14 Millis
+            Thread.sleep(300);
+        });
+    }
 }
