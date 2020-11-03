9f3feb36a4dc3ee92e8b69f3cface22a504128f2
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-11-03 17:35:39 +0900

    test : Add  annotation coment

--- src/test/java/com/example/demo/AppTest.java
+++ src/test/java/com/example/demo/AppTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.*;
 
 public class AppTest {
 
+    // 기본 테스트
     @Test
     @DisplayName("생성 테스트")
     public void create() {
         System.out.println("AppTest.create");
     }
 
+    // 깨진 테스트 Ignore
     @Test
     @Disabled
     @DisplayName("깨진 테스트")
@@ -17,27 +19,32 @@ public class AppTest {
         System.out.println("AppTest.brokenTest");
     }
 
+    // 기본 테스트 2
     @Test
     @DisplayName("조회 테스트")
     public void find() {
         System.out.println("AppTest.find");
     }
 
+    // 모든 테스트가 실행되기 이전에 1번 호출된다.
     @BeforeAll
     public static void beforeAll() {
         System.out.println("AppTest.beforeAll");
     }
 
-    @BeforeEach
-    public void beforeEach() {
-        System.out.println("AppTest.beforeEach");
-    }
-
+    // 모든 테스트가 실행된 이후에 1번 호출된다.
     @AfterAll
     public static void afterAll() {
         System.out.println("AppTest.afterAll");
     }
 
+    // 각각의 테스트가 실행되기 이전에 호출된다.
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("AppTest.beforeEach");
+    }
+
+    // 각각의 테스트가 실행된 이후에 호출된다.
     @AfterEach
     public void afterEach() {
         System.out.println("AppTest.afterEach");
